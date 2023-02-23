e93484bb741382814067edeaac340c9cd38f1159
durkes <durkes@users.noreply.github.com>
2023-02-23 13:43:21 -0600

    Update typescript examples

--- index.js
+++ index.js
@@ -1,41 +1,188 @@
-// reference the .ts file
-
-function hello(msg, x) {
-    while (x-- > 0) {
-        console.log(msg);
+"use strict";
+// typescript deep dive book
+// https://github.com/basarat/typescript-book
+// ESLint for TypeScript in VS Code setup
+// https://thesoreon.com/blog/how-to-set-up-eslint-with-typescript-in-vs-code
+// compile from CLI: tsc index
+// run directly CLI: ts-node index
+(function () {
+    function hello(msg, x) {
+        while (x-- > 0) {
+            console.log(msg);
+        }
     }
-}
-hello('Hello World', 5);
-function pow(x, y) {
-    return String(Math.pow(x, y));
-}
-console.log(pow(2, 2));
-var font = 'bold';
-console.log(font);
-var john = {
-    first: 'John',
-    last: 'Smith',
-    id: 123
-};
-console.log(john);
-var vipJohn = {
-    first: 'John',
-    last: 'Smith',
-    id: 123,
-    favoriteColor: 'blue',
-    car: 'BMW',
-    favoriteNumber: 1
-};
-console.log(vipJohn);
-var arr = [];
-arr.push(parseInt('1'));
-arr.push(2);
-var list = [1, 'john', true];
-console.log(list);
-var list2 = [];
-list2.push(1, 'john', true);
-function identity(value) {
-    return value;
-}
-var result = identity({ name: 'TypeScript' });
-console.log(result);
+    hello('Hello World', 5);
+})();
+// specify return value (instead of void above)
+(function () {
+    function pow(x, y) {
+        return String(Math.pow(x, y));
+    }
+    console.log(pow(2, 2));
+})();
+// custom type
+(function () {
+    let font = 'bold';
+    console.log(font);
+})();
+// define object
+(function () {
+    const john = {
+        first: 'John',
+        last: 'Smith',
+        id: 123
+    };
+    console.log(john);
+})();
+// define object with flexibility for custom keys
+(function () {
+    const vipJohn = {
+        first: 'John',
+        last: 'Smith',
+        id: 123,
+        favoriteColor: 'blue',
+        car: 'BMW',
+        favoriteNumber: 1
+    };
+    console.log(vipJohn);
+})();
+// strict arrays
+(function () {
+    const arr = [];
+    arr.push(parseInt('1'));
+    arr.push(2);
+})();
+// tuples
+(function () {
+    const list = [1, 'john', true];
+    console.log(list);
+})();
+// ? makes them optional, useful for empty array init
+(function () {
+    const list = [];
+    list.push(1, 'john', true);
+})();
+// generics (variable type)
+(function () {
+    function identity(value) {
+        return value;
+    }
+    const result = identity({ name: 'TypeScript' });
+    console.log(result);
+})();
+// generic interfaces
+(function () {
+    const contactForm = {
+        errors: {
+            email: "This must be a valid email address"
+        },
+        values: {
+            name: "Bob",
+            email: "bob@someemail.com",
+        },
+    };
+    console.log(contactForm);
+})();
+// generic parameter constraints
+(function () {
+    function logItems(items) {
+        items.forEach(item => item.log());
+    }
+    function getFieldValue(form, fieldName) {
+        return form.values[fieldName];
+    }
+    const contactForm = {
+        values: {
+            name: "Bob",
+            email: "bob@someemail.com",
+            phone: 8005551212
+        },
+    };
+    console.log(getFieldValue(contactForm, "name"));
+    console.log(getFieldValue(contactForm, "phone"));
+})();
+// class w/ instanceof type guard
+(function () {
+    class Contact {
+        emailAddress;
+        constructor(emailAddress) {
+            this.emailAddress = emailAddress;
+        }
+    }
+    class Person extends Contact {
+        firstName;
+        surname;
+        constructor(firstName, surname, emailAddress) {
+            super(emailAddress);
+            this.firstName = firstName;
+            this.surname = surname;
+        }
+    }
+    class Organisation extends Contact {
+        name;
+        constructor(name, emailAddress) {
+            super(emailAddress);
+            this.name = name;
+        }
+    }
+    function sayHello(contact) {
+        if (contact instanceof Person) {
+            console.log("Hello " + contact.firstName);
+        }
+        else if (contact instanceof Organisation) {
+            console.log("Hello " + contact.name);
+        }
+    }
+    const bob = new Person("Bob", "Young", "bob.young@somewhere.com");
+    const redBricks = new Organisation("Red Bricks", "info.redbricks@somewhere.com");
+    sayHello(bob);
+    sayHello(redBricks);
+})();
+// user-defined type guard with a type predicate
+(function () {
+    function isPerson(contact) {
+        return contact.firstName !== undefined;
+    }
+    function isOrganisation(contact) {
+        return contact.name !== undefined;
+    }
+    function sayHello(contact) {
+        if (isPerson(contact)) {
+            console.log("Hello " + contact.firstName);
+        }
+        else if (isOrganisation(contact)) {
+            console.log("Hello " + contact.name);
+        }
+    }
+    const bob = {
+        firstName: "Bob",
+        surname: "Young"
+    };
+    const redBricks = {
+        name: "Red Bricks"
+    };
+    sayHello(bob);
+    sayHello(redBricks);
+})();
+// mapped types
+(function () {
+    const contactForm = {
+        values: {
+            name: "Bob",
+            email: "bob@someemail.com"
+        },
+        errors: {
+            email: "Invalid email address"
+        }
+    };
+    console.log(contactForm);
+})();
+// conditional types
+(function () {
+})();
+// utility types
+(function () {
+    // check whether  type contains null: null extends T[P]
+    // if true, return prop name, which is P
+    // If false, return never to remove this from the constructed type
+})();
--- index.ts
+++ index.ts
@@ -7,92 +7,295 @@
 // compile from CLI: tsc index
 // run directly CLI: ts-node index
 
-
-function hello(msg: string, x: number): void {
-    while (x-- > 0) {
-        console.log(msg);
+(function () {
+    function hello(msg: string, x: number): void {
+        while (x-- > 0) {
+            console.log(msg);
+        }
     }
-}
 
-hello('Hello World', 5);
+    hello('Hello World', 5);
+})();
 
 
 // specify return value (instead of void above)
-function pow(x: number, y: number): string {
-    return String(Math.pow(x, y));
-}
+(function () {
+    function pow(x: number, y: number): string {
+        return String(Math.pow(x, y));
+    }
 
-console.log(pow(2, 2));
+    console.log(pow(2, 2));
+})();
 
 
 // custom type
-type Style = 'bold' | 'italic';
-let font: Style = 'bold';
+(function () {
+    type Style = 'bold' | 'italic';
+    let font: Style = 'bold';
 
-console.log(font);
+    console.log(font);
+})();
 
 
 // define object
-interface Employee {
-    first: string,
-    last: string,
-    id: number
-}
+(function () {
+    interface Employee {
+        first: string,
+        last: string,
+        id: number
+    }
 
-const john: Employee = {
-    first: 'John',
-    last: 'Smith',
-    id: 123
-};
+    const john: Employee = {
+        first: 'John',
+        last: 'Smith',
+        id: 123
+    };
 
-console.log(john);
+    console.log(john);
+})();
 
 
 // define object with flexibility for custom keys
-interface VIP {
-    first: string,
-    last: string,
-    id: number,
-    [key: string]: any
-}
+(function () {
+    interface VIP {
+        first: string,
+        last: string,
+        id: number,
+        [key: string]: any
+    }
 
-const vipJohn: VIP = {
-    first: 'John',
-    last: 'Smith',
-    id: 123,
-    favoriteColor: 'blue',
-    car: 'BMW',
-    favoriteNumber: 1
-};
+    const vipJohn: VIP = {
+        first: 'John',
+        last: 'Smith',
+        id: 123,
+        favoriteColor: 'blue',
+        car: 'BMW',
+        favoriteNumber: 1
+    };
 
-console.log(vipJohn);
+    console.log(vipJohn);
+})();
 
 
 // strict arrays
-const arr: number[] = [];
-arr.push(parseInt('1'));
-arr.push(2);
+(function () {
+    const arr: number[] = [];
+    arr.push(parseInt('1'));
+    arr.push(2);
+})();
 
 
 // tuples
-type MyList = [number, string, boolean];
-const list: MyList = [1, 'john', true];
-console.log(list);
+(function () {
+    type MyList = [number, string, boolean];
+    const list: MyList = [1, 'john', true];
+    console.log(list);
+})();
+
 
 // ? makes them optional, useful for empty array init
-type MyList2 = [number?, string?, boolean?];
-const list2: MyList2 = [];
-list2.push(1, 'john', true);
+(function () {
+    type MyList = [number?, string?, boolean?];
+    const list: MyList = [];
+    list.push(1, 'john', true);
+})();
 
 
 // generics (variable type)
-type ProgrammingLanguage = {
-    name: string;
-};
+(function () {
+    type ProgrammingLanguage = {
+        name: string;
+    };
 
-function identity<T>(value: T): T {
-    return value;
-}
+    function identity<T>(value: T): T {
+        return value;
+    }
 
-const result = identity<ProgrammingLanguage>({ name: 'TypeScript' });
-console.log(result);
+    const result = identity<ProgrammingLanguage>({ name: 'TypeScript' });
+    console.log(result);
+})();
+
+
+// generic interfaces
+(function () {
+    interface Form<T> {
+        errors: {
+            [K in keyof T]?: string;
+        };
+        values: T;
+    }
+    // same, but as type
+    // type Form<T> = {
+    //     errors: { [K in keyof T]?: string };
+    //     values: T;
+    // };
+
+    interface Contact {
+        name: string;
+        email: string;
+    }
+
+    const contactForm: Form<Contact> = {
+        errors: {
+            email: "This must be a valid email address"
+        },
+        values: {
+            name: "Bob",
+            email: "bob@someemail.com",
+        },
+    };
+
+    console.log(contactForm);
+})();
+
+
+// generic parameter constraints
+(function () {
+    interface Logable {
+        log: () => void;
+    }
+    function logItems<T extends Logable>(items: T[]): void {
+        items.forEach(item => item.log());
+    }
+
+    interface Form<T> {
+        values: T;
+    }
+    function getFieldValue<T, K extends keyof T>(
+        form: Form<T>, fieldName: K) {
+        return form.values[fieldName];
+    }
+
+    const contactForm = {
+        values: {
+            name: "Bob",
+            email: "bob@someemail.com",
+            phone: 8005551212
+        },
+    };
+    console.log(getFieldValue(contactForm, "name"));
+    console.log(getFieldValue(contactForm, "phone"));
+})();
+
+
+// class w/ instanceof type guard
+(function () {
+    class Contact {
+        constructor(public emailAddress: string) { }
+    }
+    class Person extends Contact {
+        constructor(
+            public firstName: string,
+            public surname: string,
+            emailAddress: string
+        ) {
+            super(emailAddress);
+        }
+    }
+    class Organisation extends Contact {
+        constructor(public name: string, emailAddress: string) {
+            super(emailAddress);
+        }
+    }
+
+    function sayHello(contact: Contact) {
+        if (contact instanceof Person) {
+            console.log("Hello " + contact.firstName);
+        }
+        else if (contact instanceof Organisation) {
+            console.log("Hello " + contact.name)
+        }
+    }
+
+    const bob = new Person("Bob", "Young", "bob.young@somewhere.com");
+    const redBricks = new Organisation(
+        "Red Bricks",
+        "info.redbricks@somewhere.com"
+    );
+
+    sayHello(bob);
+    sayHello(redBricks);
+})();
+
+
+// user-defined type guard with a type predicate
+(function () {
+    interface Person {
+        firstName: string;
+        surname: string;
+    }
+    interface Organisation {
+        name: string;
+    }
+    type Contact = Person | Organisation;
+
+    function isPerson(contact: Contact): contact is Person {
+        return (contact as Person).firstName !== undefined;
+    }
+
+    function isOrganisation(contact: Contact): contact is Organisation {
+        return (contact as Organisation).name !== undefined;
+    }
+
+    function sayHello(contact: Contact) {
+        if (isPerson(contact)) {
+            console.log("Hello " + contact.firstName);
+        }
+        else if (isOrganisation(contact)) {
+            console.log("Hello " + contact.name);
+        }
+    }
+
+    const bob: Person = {
+        firstName: "Bob",
+        surname: "Young"
+    };
+
+    const redBricks: Organisation = {
+        name: "Red Bricks"
+    };
+
+    sayHello(bob);
+    sayHello(redBricks);
+})();
+
+
+// mapped types
+(function () {
+    interface Form<T> {
+        values: T;
+        errors: { [K in keyof T]?: T[K] };
+    }
+
+    const contactForm: Form<{ name: string; email: string }> = {
+        values: {
+            name: "Bob",
+            email: "bob@someemail.com"
+        },
+        errors: {
+            email: "Invalid email address"
+        }
+    };
+    console.log(contactForm);
+})();
+
+
+// conditional types
+(function () {
+    type Animal = {
+        name: string;
+        legs: number
+    }
+    type Check = Animal extends { name: string } ? "yes" : "no"; // "yes"
+})();
+
+
+// utility types
+(function () {
+    type NullableProps<T> = {
+        [P in keyof T]: null extends T[P] ? P : never
+    }[keyof T];
+    // check whether  type contains null: null extends T[P]
+    // if true, return prop name, which is P
+    // If false, return never to remove this from the constructed type
+})();
